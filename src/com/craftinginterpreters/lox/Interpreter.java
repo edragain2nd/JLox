c52b5867c26fda93df1ce252d8c51ed730400aab
your Name <you@example.com>
2024-10-19 03:24:03 +0800

    截至到11，修复函数漏洞，有点困，可能犯错多

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,11 +1,14 @@
 package com.craftinginterpreters.lox;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 class Interpreter implements Expr.Visitor<Object>,
         Stmt.Visitor<Void> {
 
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
     Interpreter() {
         globals.define("clock", new LoxCallable() {
             @Override
@@ -71,7 +74,15 @@ class Interpreter implements Expr.Visitor<Object>,
     }
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
+    }
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            return environment.getAt(distance, name.lexeme);
+        } else {
+            return globals.get(name);
+        }
     }
     private boolean isTruthy(Object object) {
         if (object == null) return false;
@@ -102,6 +113,9 @@ class Interpreter implements Expr.Visitor<Object>,
 }
     private void execute(Stmt stmt) {
         stmt.accept(this);
+    }
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
     }
     void executeBlock(List<Stmt> statements,
                       Environment environment) {
@@ -169,7 +183,12 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            environment.assignAt(distance, expr.name, value);
+        } else {
+            globals.assign(expr.name, value);
+        }
         return value;
     }
     @Override
--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -52,7 +52,8 @@ public class Lox {
 
         // Stop if there was a syntax error.
         if (hadError) return;
-
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
         interpreter.interpret(statements);
     }
     static void error(int line, String message) {
